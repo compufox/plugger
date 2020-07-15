fd50ccf2e30444221f25c5ef16de935f978e31f8
Zac <thezacattacks@gmail.com>
2020-07-15 12:22:05 -0400

    added :str dependency
    replaced uiop:strcat with str:concat
    replaced replace-char with str:replace-all

--- plugger.asd
+++ plugger.asd
@@ -6,6 +6,6 @@
   :license  "BSD 3-Clause"
   :version "0.0.1"
   :serial t
-  :depends-on (#:dexador #:json-mop)
+  :depends-on (#:dexador #:json-mop #:str)
   :components ((:file "package")
                (:file "plugger")))
--- plugger.lisp
+++ plugger.lisp
@@ -4,24 +4,16 @@
 
 (defvar *api-root* "/api/v1/")
 
-(defun replace-char (o n s)
-  (let ((place (search o s :test #'string=)))
-    (if place
-	(replace-char o n
-		      (uiop:strcat (subseq s 0 place)
-				   n
-				   (subseq s (1+ place))))
-	s)))
 
 (defmacro defplug (domain path &key return-type (methods (list :get)) trailing-argument)
   `(progn
-     ,@(loop with converted-path = (replace-char "/" "-" path)
+     ,@(loop with converted-path = (str:replace-all "/" "-" path)
 	     for method in methods
 	     for func = (intern (string method) :dexador)
 	     collect 
 	     `(defun ,(intern (string-upcase
-			       (uiop:strcat (string method) "-"
-					    converted-path)))
+			       (str:concat (string method) "-"
+					   converted-path)))
 		  ,(append nil
 		    (when trailing-argument
 		      '(argument))
@@ -30,8 +22,8 @@
 		      '(data))
 		    '(&key (root *api-root*) headers basic-auth))
 		(json-mop:json-to-clos ,(append `(funcall #',func)
-						`((uiop:strcat ,domain root ,path
-							       ,(when (and trailing-argument
+						`((str:concat ,domain root ,path
+							      ,(when (and trailing-argument
 									   (eql method :get))
 								  '(format nil "/~A" argument))))
 						'(:headers headers :basic-auth basic-auth)
